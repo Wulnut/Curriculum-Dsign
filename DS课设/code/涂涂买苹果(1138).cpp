d9c15828e4804e6cd46f15ea60dbf692f145b3fa
Wulnut <985951964@qq.com>
2019-12-20 08:38:26 +0800

    12-20 last commit

--- DS课设/code/涂涂买苹果(1138).cpp
+++ DS课设/code/涂涂买苹果(1138).cpp
@@ -1,34 +1,23 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
-    int n, i, p = 0, j = 0, count = 0;
-    long long m, all = 0;
+    int n;//apple number
     cin >> n;
-    long long a[n+1];
-    for(int i=0; i < n; ++i){
-        cin >> a[i];
+    long long ai[n];//apple KG
+    long long m;
+    bool flag = true;
+    for(int i = 0; i < n; ++i){
+        cin >> ai[i];
     }
-    cin>>m;
-    for(i = 0; i < n; ++i){
-        all += a[i];
+
+    cin >> m;
+    sort(ai, ai + n);
+
+    for(int i = n - 1; i < n; i++){
+        m = m - ai[i];
+        count++;
     }
-    if(all >= m){
-        while(m > 0){
-            p = 1;
-            for(i = 0; i < n; ++i){
-                if(a[i] >= p){
-                    p = a[i];
-                }
-                j = i;
-            }
-            m = m - p;
-            a[j] = 0;
-            count++;
-        }
-        cout << count;
-    }
-    else{
-        cout << 0;
-    }
+
     return 0;
 }
